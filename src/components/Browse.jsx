70536134b0c8a9296409834b3eb1a0774c2963a1
joshi-durgesh <durgesh7joshi9@gmail.com>
2024-02-29 22:33:01 +0530

    get data from tmdb website and created hook for it to fetch api and store data at redux store

--- src/components/Browse.jsx
+++ src/components/Browse.jsx
@@ -1,18 +1,8 @@
-import { useEffect } from "react";
-import { API_OPTIONS, API_URL } from "../utils/constant";
+import useNowPlayingMovies from "../hooks/useNowPlayingMovies";
 import BrowseHeader from "./BrowseHeader";
 
 const Browse = () => {
-  const getNowPlayingMovies = async () => {
-    const data = await fetch(API_URL, API_OPTIONS);
-    const json = await data.json();
-    console.log(json);
-  };
-
-  useEffect(() => {
-    getNowPlayingMovies();
-  }, []);
-
+  useNowPlayingMovies();
   return (
     <section>
       <BrowseHeader />
---
+++ src/hooks/useNowPlayingMovies.jsx
@@ -0,0 +1,19 @@
+import { useDispatch } from "react-redux";
+import { API_OPTIONS, API_URL } from "../utils/constant";
+import { addNowPlayingMovies } from "../utils/moviesSlice";
+
+const useNowPlayingMovies = () => {
+  const dispatch = useDispatch();
+
+  const getNowPlayingMovies = async () => {
+    const data = await fetch(API_URL, API_OPTIONS);
+    const json = await data?.json();
+    dispatch(addNowPlayingMovies(json?.results));
+  };
+
+  useEffect(() => {
+    getNowPlayingMovies();
+  }, []);
+};
+
+export default useNowPlayingMovies;
