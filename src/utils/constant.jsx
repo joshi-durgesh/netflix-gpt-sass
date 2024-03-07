f95a24a72e99c75bc674d14d9ae5a3a3eb8ba04a
joshi-durgesh <durgesh7joshi9@gmail.com>
2024-03-07 23:20:47 +0530

    change key in constant for open ai

--- src/utils/constant.jsx
+++ src/utils/constant.jsx
@@ -39,4 +39,4 @@ export const API_OPTIONS = {
 
 export const IMG_CDN_URL = "https://image.tmdb.org/t/p/w500";
 
-export const OPENAI_KEY = "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";
+export const OPENAI_KEY = "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";
